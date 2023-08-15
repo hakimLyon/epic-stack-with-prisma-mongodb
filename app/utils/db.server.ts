a49f15db5e40a663b6673d99340d594cac56e10f
Hakim Gueye <64654633+hakimLyon@users.noreply.github.com>
2023-08-15 20:31:23 +0000

    Update db.server.ts

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,7 +1,9 @@
 import { PrismaClient } from '@prisma/client'
 import { singleton } from './singleton.server.ts'
 
-const prisma = singleton('prisma', () => new PrismaClient())
-prisma.$connect()
-
+const prisma = singleton('prisma', () => {
+	const client = new PrismaClient()
+	client.$connect() // Make sure to call $connect after creating the instance
+	return client
+})
 export { prisma }
